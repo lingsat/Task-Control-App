e994152ef05c9fba8a7b802f0deab81129dacbe0
lingsat <lingsatpilingast@gmail.com>
2022-10-16 10:02:30 +0300

    test: add tests for directives in feature module:

--- src/app/features/directives/board-highlight.directive.spec.ts
+++ src/app/features/directives/board-highlight.directive.spec.ts
@@ -1,16 +1,39 @@
-import { ElementRef } from '@angular/core';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { DebugElement } from '@angular/core';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormsModule } from '@angular/forms';
+import { By } from '@angular/platform-browser';
+import { DashboardComponent } from '../components/dashboard/dashboard.component';
+import { FilteringPipe } from '../pipes/filtering.pipe';
+import { SortingPipe } from '../pipes/sorting.pipe';
 import { BoardHighlightDirective } from './board-highlight.directive';
 
 describe('BoardHighlightDirective', () => {
-  const mockElementRef: any = {
-    nativeElement: {
-      style: {
-        backgroundColor: '#D2D2D4'
-      }  
-    }
-  };
-  it('should create an instance', () => {
-    const directive = new BoardHighlightDirective(mockElementRef);
-    expect(directive).toBeTruthy();
+  let fixture: ComponentFixture<DashboardComponent>;
+  let debugElement: DebugElement;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [BoardHighlightDirective, DashboardComponent, FilteringPipe, SortingPipe],
+      imports: [HttpClientTestingModule, FormsModule],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(DashboardComponent);
+    fixture.detectChanges();
+    debugElement = fixture.debugElement;
+  });
+
+  it('hover element', () => {
+    const divEl = debugElement.query(By.css('#addBoardBtn'));
+    divEl.triggerEventHandler('mouseenter', null);
+    expect(divEl.nativeElement.style.backgroundColor).toBeTruthy();
+    expect(divEl.nativeElement.style.backgroundColor).toBe('rgb(210, 210, 212)');
+  });
+
+  it('leave element', () => {
+    const divEl = debugElement.query(By.css('#addBoardBtn'));
+    divEl.triggerEventHandler('mouseleave', null);
+    expect(divEl.nativeElement.style.backgroundColor).toBeTruthy();
+    expect(divEl.nativeElement.style.backgroundColor).toBe('rgb(231, 234, 239)');
   });
 });
--- src/app/features/directives/click-outside.directive.spec.ts
+++ src/app/features/directives/click-outside.directive.spec.ts
@@ -1,15 +1,39 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { DebugElement, ElementRef } from '@angular/core';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormsModule } from '@angular/forms';
+import { By } from '@angular/platform-browser';
+import { fromEvent } from 'rxjs';
+import { AddBoardFormComponent } from '../components/dashboard/forms/add-board-form/add-board-form.component';
 import { ClickOutsideDirective } from './click-outside.directive';
 
-describe('ClickOutsideDirective', () => {
-  const mockElementRef: any = {
-    nativeElement: {
-      style: {
-        backgroundColor: '#D2D2D4'
-      }  
-    }
-  };
+describe('BoardHighlightDirective', () => {
+  let fixture: ComponentFixture<AddBoardFormComponent>;
+  let addBoardComponent: AddBoardFormComponent;
+  let debugElement: DebugElement;
+  let backgroundEl: ElementRef;
+  let clickOutsideDirective: ClickOutsideDirective;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ClickOutsideDirective, AddBoardFormComponent],
+      imports: [HttpClientTestingModule, FormsModule],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(AddBoardFormComponent);
+    fixture.detectChanges();
+    addBoardComponent = fixture.componentInstance;
+    debugElement = fixture.debugElement;
+    backgroundEl = debugElement.query(By.css('.background'));
+    clickOutsideDirective = new ClickOutsideDirective(backgroundEl, document);
+  });
+
   it('should create an instance', () => {
-    const directive = new ClickOutsideDirective(mockElementRef, document);
-    expect(directive).toBeTruthy();
+    expect(clickOutsideDirective).toBeTruthy();
+  });
+
+  it('click inside', () => {
+    let isClickedInside = clickOutsideDirective.isInside(backgroundEl.nativeElement);
+    expect(isClickedInside).toBeTrue();
   });
 });
